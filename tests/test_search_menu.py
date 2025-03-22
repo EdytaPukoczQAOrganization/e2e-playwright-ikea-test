50e28aa106386dafb314dcc7bff52e1ace571900
Edyta Pukocz <edytasanchez@gmail.com>
2025-03-22 19:00:11 +0100

    Update test_search_menu
    Update automated tests

--- tests/test_search_menu.py
+++ tests/test_search_menu.py
@@ -1,4 +1,5 @@
 from playwright.sync_api import Page, expect
+import re
 
 def test_search_valid_value(page:Page):
     print("Given the user is on the Ikea homepage")
@@ -9,12 +10,18 @@ def test_search_valid_value(page:Page):
     
     print("And the user introduces a valid search value silla")
     #We use the .clear option to clear the search box in case there was something in there before
-    page.get_by_role("combobox", name="Buscar por producto,").clear()
+    ##page.get_by_role("combobox", name="Buscar por producto,").clear()
+    page.get_by_placeholder("Qué es lo que buscas").click()
     ##page.get_by_role("combobox", name="Buscar por producto,").fill("silla")
     page.get_by_placeholder("Qué es lo que buscas").fill("silla")
     
     print("And the user hits enter")
-    page.get_by_role("button", name="Buscar").click()
+    ##page.get_by_role("button", name="Buscar").click()
+    ##page.get_by_role("tooltip", name="Buscar").click()
+    page.locator("span").filter(has_text=re.compile(r"^Buscar$")).click()
+    
+
+
     
     print("Then the user should see the search results with the search term silla as the H1")
     expect(page.locator("h1")).to_contain_text("silla")
@@ -27,8 +34,11 @@ def test_search_empty_value(page:Page):
     page.get_by_role("button", name="Aceptar todas").click()
     
     print("And the user introduces an empty value and press enter")
+
     page.get_by_role("combobox", name="Buscar por producto,").click()
-    page.get_by_role("combobox", name="Buscar por producto,").press("Enter")
+    ##page.get_by_role("combobox", name="Buscar por producto,").press("Enter")
+    page.get_by_placeholder("Qué es lo que buscas").fill(" ")
+    page.locator("span").filter(has_text=re.compile(r"^Buscar$")).click()
     
     print("Then the user should still be seeing the search box with Buscar por producto text")
     expect(page.get_by_role("combobox", name="Buscar por producto,")).to_be_visible()
